417b2cb8c3617bbad68199d720d582d3b83fff4e
zxkuba <jakub.krzeszowiak@gmail.com>
2020-05-06 19:41:30 +0200

    zadanie 21.4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,19 +1,27 @@
 package com.crud.tasks;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//@SpringBootApplication
+//public class TasksApplication extends SpringBootServletInitializer
+
 @SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {
-    public class TasksApplication{
+public class TasksApplication{
+
 
     public static void main(String[] args) {
         SpringApplication.run(TasksApplication.class, args);
     }
-//@Override
-  //  protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-   //     return application.sources(TasksApplication.class);
+
+ //@Override
+   //protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+     //return application.sources(TasksApplication.class);
+    }
 //}
-}
+
+
+
